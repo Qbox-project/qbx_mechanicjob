9a07bf61969eed6a8a285031781ce32948c1335b
mockdot <lucasge@me.com>
2023-07-20 16:41:59 -0600

    fixes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,25 +5,25 @@ description 'QBX-MechanicJob'
 version '1.0.0'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'locales/es.lua',
-	'locales/*.lua',
-	'config.lua',
-	'@ox_lib/init.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/es.lua',
+    'locales/*.lua',
+    'config.lua',
+    '@ox_lib/init.lua',
 }
 
 client_scripts {
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/CircleZone.lua',
-	'client/damage-effects.lua',
-	'client/main.lua',
-	'client/drivingdistance.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/CircleZone.lua',
+    'client/damage-effects.lua',
+    'client/main.lua',
+    'client/drivingdistance.lua',
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server/main.lua'
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
 }
 
 dependencies {
